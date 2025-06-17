cb1f5a602c3e3df83913c5962699fc6753d3cd7a
kimtuyenngthi <20130147@st.hcmuaf.edu.vn>
2025-06-18 00:36:07 +0700

    update user admin

--- bookstore_fe/src/admin/UserManagement.tsx
+++ bookstore_fe/src/admin/UserManagement.tsx
@@ -10,7 +10,7 @@ const UserManagement = () => {
 	// Tạo ra biến để mỗi khi thao tác CRUD thì sẽ update lại table
 	const [keyCountReload, setKeyCountReload] = useState(0);
 
-	const [option, setOption] = useState(""); // Truyền vào là có thể là (add, update)
+	const [option, setOption] = useState(""); //Truyền vào là có thể là (add, update)
 	const [openModal, setOpenModal] = React.useState(false);
 	const handleOpenModal = () => setOpenModal(true);
 	const handleCloseModal = () => setOpenModal(false);
@@ -18,45 +18,43 @@ const UserManagement = () => {
 	const [id, setId] = useState<number>(0);
 
 	return (
-		<div className='conatiner p-5'>
-			<div className='shadow-4-strong rounded p-5'>
-				<div className='mb-3'>
-					<Button
-						variant='contained'
-						color='success'
-						onClick={() => {
-							handleOpenModal();
-							setOption("add");
-						}}
-						startIcon={<AddIcon />}
-					>
-						Thêm người dùng
-					</Button>
-				</div>
-				<div>
-					<UserTable
-						keyCountReload={keyCountReload}
-						setOption={setOption}
-						handleOpenModal={handleOpenModal}
-						setKeyCountReload={setKeyCountReload}
-						setId={setId}
-					/>
-				</div>
+		<div className='shadow-4-strong rounded p-5'>
+			<div className='mb-3'>
+				<Button
+					variant='contained'
+					color='success'
+					onClick={() => {
+						handleOpenModal();
+						setOption("add");
+					}}
+					startIcon={<AddIcon />}
+				>
+					Thêm người dùng
+				</Button>
 			</div>
-			<FadeModal
-				open={openModal}
-				handleOpen={handleOpenModal}
-				handleClose={handleCloseModal}
-			>
-				<UserForm
-					option={option}
+			<div>
+				<UserTable
+					keyCountReload={keyCountReload}
+					setOption={setOption}
+					handleOpenModal={handleOpenModal}
 					setKeyCountReload={setKeyCountReload}
-					id={id}
-					handleCloseModal={handleCloseModal}
+					setId={setId}
 				/>
-			</FadeModal>
+			</div>
 		</div>
-	);
+	<FadeModal
+		open={openModal}
+		handleOpen={handleOpenModal}
+		handleClose={handleCloseModal}
+	>
+		<UserForm
+			option={option}
+			setKeyCountReload={setKeyCountReload}
+			id={id}
+			handleCloseModal={handleCloseModal}
+		/>
+	</FadeModal>
+);
 };
 
 const UserManagementPage = RequireAdmin(UserManagement);
